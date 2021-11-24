7923f2bc3b384203e39da538f04b348915c1a34a
ted <ted@warpedpuppy.com>
2021-11-24 14:42:09 -0500

    refactored class into function

--- WidgetList.js
+++ WidgetList.js
@@ -1,39 +1,51 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import Faker from 'faker';
+import './WidgetList.css';
 
-class WidgetList extends Component{
-  state = {
-    widgets: [],
-    loading: true,
-  }
+function WidgetList(props) {
 
-  function handleSaveWidgets() {
-    this.props.saveWidgets(this.state)
-  }
+  const [loading, setLoading] = useState(true);
 
-  render(){
-    return(
-      <div>
-        {this.state.loading && (<div>Loading...</div>)}
-        {this.state.widgets.each(function(widget) {
-          <h1>{widget.title}</h1>
-          <p>{widget.text}</p>
-        })}
-        <button onClick={this.handleSaveWidgets}>SAVE</button>
-      </div>
-    )
-  }
-
-  const mapStateToProps = state => {
-    return {
-      widgets: state.widgets,
+  useEffect( () => {
+    if (props.widgets.length) {
+      setLoading(false)
     }
+  }, [props.widgets])
+ 
+  function handleSaveWidgets () {
+    let title = `Title #${Math.ceil(Math.random()*1000)}`;
+    let text = Faker.lorem.sentence();
+    props.saveWidgets({title, text})
   }
 
-  const mapDispatchToProps = dispatch => {
-    return {
-      saveWidgets: (widgets) => dispatch({type: "SAVE_WIDGETS", widgets})
-    }
+  return  (
+    <div>
+      <button className="add-widget" onClick={ handleSaveWidgets }>add widget</button>
+      <section id="widget-display">
+        { loading && (<div className="loading">the widget list is empty right now. . . </div>) }
+        {
+          props.widgets.map((widget, i) =>  {
+            return (
+              <div key={i}>
+                <h1>{ widget.title }</h1>
+                <p>{ widget.text }</p>
+              </div>
+            )
+          })
+        }
+      </section>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  const { widgets } = state;
+  return { widgets }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    saveWidgets: (widgets) => dispatch({type: "SAVE_WIDGETS", value: widgets})
   }
 }
 
